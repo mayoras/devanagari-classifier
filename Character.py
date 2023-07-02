7f95fbdd210e7e987802e5e6ce375ba1a6ef5ae4
Cesar Mayora <cesarmay@protonmail.com>
2023-07-02 22:33:58 +0200

    Character class

---
+++ Character.py
@@ -0,0 +1,13 @@
+from joblib import load
+import numpy as np
+from PIL import Image
+
+
+class Character:
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.img_arr = self._read_image(filename)
+
+    def _read_image(filename: str) -> np.ndarray(dtype=np.int32):
+        pil_img = Image.open(filename, "r")
+        return np.array(pil_img, dtype=np.int32)
