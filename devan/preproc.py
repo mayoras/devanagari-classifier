4b993d32123a76404113fd31eccbccccdf5c3fce
Cesar Mayora <cesarmay@protonmail.com>
2023-07-05 16:40:02 +0200

    List of characters compatibility with one character

--- devan/preproc.py
+++ devan/preproc.py
@@ -1,27 +1,64 @@
-import matplotlib.pyplot as plt
+import numpy as np
 from skimage.feature import hog
+
+from devan.character import Character
 
 BYTE_SIZE = 255
 
-# TODO: make compatible with a group of characters
 
-
-def min_max_scaling(arr):
+def byte_normalize(arr):
     return arr / BYTE_SIZE
 
 
-def get_hog_desc(X):
-    # reconvert feature vector in a matrix (image)
-    X_aux = X.reshape(32, 32)
+def min_max_scaling(X, min_vals=None, max_vals=None):
+    if X.size == 1 or not min_vals or not max_vals:
+        print("Warning: using byte-normalization because there's only one example")
+        return byte_normalize(X)
 
-    # return the HOG descriptor of the image
-    return hog(
-        X_aux,
-        orientations=8,
-        pixels_per_cell=(8, 8),
-        cells_per_block=(1, 1),
-        block_norm="L2-Hys",
-    )
+    norm = np.zeros(X.shape, dtype=float)
+
+    zero_cols = np.where(np.all(X == 0, axis=0))[0]
+
+    # for each column
+    for i in range(X.shape[1]):
+        if i in zero_cols:
+            norm[:, i] = X[:, i]
+        else:
+            norm[:, i] = (X[:, i] - min_vals[i]) / (max_vals[i] - min_vals[i])
+
+    return norm
+
+
+def get_hog_desc(X):
+    # is a single image
+    if len(X.shape) < 2:
+        # reconvert feature vector in a matrix (image)
+        X_aux = X.reshape(32, 32)
+
+        # return the HOG descriptor of the image
+        return hog(
+            X_aux,
+            orientations=8,
+            pixels_per_cell=(8, 8),
+            cells_per_block=(1, 1),
+            block_norm="L2-Hys",
+        )
+    else:
+        X_aux = X.reshape(X.shape[0], 32, 32)
+        # por cada ejemplo, obtenemos su respectivo descriptor HOG
+        fds = []
+        for e in X_aux:
+            fd = hog(
+                e,
+                orientations=8,
+                pixels_per_cell=(8, 8),
+                cells_per_block=(1, 1),
+                block_norm="L2-Hys",
+            )
+            fds.append(fd)
+
+        # devolver el nuevo conjunto de train
+        return np.array(fds)
 
 
 def transform_input(arr):
