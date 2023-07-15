7fd1930e41727f34cb0da10e7012e5c49e95b5e9
Cesar Mayora <cesarmay@protonmail.com>
2023-07-15 10:33:19 +0200

    fix pipeline transformations

--- devan/pipeline.py
+++ devan/pipeline.py
@@ -1,24 +1,25 @@
 import numpy as np
-from devan.character import Character
 
-# TODO: refine transformations to be more parameterizable
+from typing import Callable
+from devan.character import Character
 
 
 class Pipeline:
-    def __init__(self, chars: list[Character]):
-        self.X = np.array([c.img_arr for c in chars], dtype=float)
-        self.trans = []
-
-    def __init__(self, chars: list[Character], trans: list[callable]):
+    # transformation === (func, {param1: p1, param2: p2})
+    def __init__(self, chars: list[Character], trans: list[tuple[Callable, dict]] = []):
+        # init prediction set and transformations
         self.X = np.array([c.img_arr for c in chars], dtype=float)
         self.trans = trans
+
+    def add_transform(self, trans: tuple[Callable, dict]):
+        self.trans.append(trans)
 
     def transform(self):
         # reshape the images to 1D-array of pixels
         X_aux = self.X.reshape(self.X.shape[0], -1)
 
         # Apply transformations
-        for t in self.trans:
-            X_aux = t(X_aux)
+        for fun, params in self.trans:
+            X_aux = fun(**params)
 
         return X_aux
