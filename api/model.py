2a9be621a432d3ab2a7a29f42069f587fc56bd22
Cesar Mayora <cesarmay@protonmail.com>
2023-08-05 16:25:38 +0200

    image body mode strict typing

--- api/model.py
+++ api/model.py
@@ -1,8 +1,9 @@
+from typing import Literal
 from pydantic import BaseModel
 
 
 class ImageBody(BaseModel):
     file: str
-    mode: str = "gray"
+    mode: Literal["gray", "rgb"] = "gray"
     alpha: bool = True
     data: str
