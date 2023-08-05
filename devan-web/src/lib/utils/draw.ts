13141b5fff128dc1d47379c0e2150844370d26b1
Cesar Mayora <cesarmay@protonmail.com>
2023-08-05 22:06:50 +0200

    doc annotation

--- devan-web/src/lib/utils/draw.ts
+++ devan-web/src/lib/utils/draw.ts
@@ -1,4 +1,11 @@
 // TODO: change this function to mimic the blurred effect without alpha values
+/**
+ * Draws a white rectangle into the canvas with blurred effect.
+ * @param ctx rendering 2D context of the canvas containing the drawing
+ * @param x canvas X-coordinate of the rectangle to be drawn
+ * @param y canvas Y-coordinate of the rectangle to be drawn
+ * @param size side length of the rectangle to be drawn
+ */
 export function drawBlurredRect(
 	ctx: CanvasRenderingContext2D | null,
 	x: number,
