16baba4841af4d1950228da236f252aecdf51c18
Cesar Mayora <cesarmay@protonmail.com>
2023-08-06 18:03:55 +0200

    avoid magic numbers

--- devan-web/src/lib/constants/image.ts
+++ devan-web/src/lib/constants/image.ts
@@ -4,3 +4,4 @@ export const DRAW_POINTER_SIZE = 13;
 export const RED_GRAY_PROPORTION = 0.2989;
 export const GREEN_GRAY_PROPORTION = 0.587;
 export const BLUE_GRAY_PROPORTION = 0.114;
+export const RGBA_PIXEL_SIZE = 4;
