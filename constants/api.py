a676a5a4b0eece43a4b7ff387bcc05e0c8ba35be
Cesar Mayora <cesarmay@protonmail.com>
2023-08-08 19:24:48 +0200

    add constants modules

---
+++ constants/api.py
@@ -0,0 +1,5 @@
+import os
+
+DEVAN_API_PORT = int(os.getenv("DEVAN_API_PORT") or 8080)
+DEVAN_API_HOSTNAME = os.getenv("DEVAN_API_HOSTNAME") or "locahost"
+DEVAN_PROD_ENV = True if os.getenv("DEVAN_PROD_ENV") == "1" else False
---
+++ constants/image.py
@@ -0,0 +1,4 @@
+from typing import Literal
+
+NORM_IMG_WIDTH = 32
+NORM_IMG_HEIGHT = 32
