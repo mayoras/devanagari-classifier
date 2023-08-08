957fdc356fa3533c63ac50aa36367002cce4f70d
Cesar Mayora <cesarmay@protonmail.com>
2023-08-08 23:51:06 +0200

    fix naming

--- test/api/test_image.py
+++ test/api/test_image.py
@@ -1,37 +1,28 @@
 import unittest
 import api.image as img
 
-from api.model import ImageBody
-
-DATA_FILENAME = "./test/api/data.txt"
-
-fake_payload = {
-    "file": "image",
-    "mode": "gray",
-    "alpha": False,
-}
-
-with open(DATA_FILENAME, "r") as file:
-    fake_payload["data"] = file.read()
-
-fake_image_body = ImageBody(**fake_payload)
-
-IMG_WIDTH = 32
-IMG_HEIGHT = 32
+from constants.image import NORM_IMG_HEIGHT, NORM_IMG_WIDTH
+from test.mocks.api.image import get_mock_image_body
 
 
 class TestImage(unittest.TestCase):
     def test_parse_img(self):
+        fake_image_body = get_mock_image_body()
+
         pil_img = img.parse_image(payload=fake_image_body)
 
         pil_img.show()
 
         self.assertIsNotNone(pil_img, msg="PILLOW Image should not be None")
         self.assertEquals(
-            pil_img.width, IMG_WIDTH, msg=f"Image should have width {IMG_WIDTH}"
+            pil_img.width,
+            NORM_IMG_WIDTH,
+            msg=f"Image should have width {NORM_IMG_WIDTH}",
         )
         self.assertEquals(
-            pil_img.height, IMG_HEIGHT, msg=f"Image should have height {IMG_HEIGHT}"
+            pil_img.height,
+            NORM_IMG_HEIGHT,
+            msg=f"Image should have height {NORM_IMG_HEIGHT}",
         )
         self.assertEquals(
             pil_img.mode, "L", msg=f"Image mode should be grayscale (8-bit pixels)"
--- test/mocks/api/image.py
+++ test/mocks/api/image.py
@@ -3,7 +3,7 @@ from api.model import ImageBody
 DATA_FILENAME = "./test/mocks/api/data.txt"
 
 
-def get_mock_character() -> ImageBody:
+def get_mock_image_body() -> ImageBody:
     fake_payload = {
         "file": "image",
         "mode": "gray",
