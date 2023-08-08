271be7f57142af2cc7f5a561c3d192b85467e4de
Cesar Mayora <cesarmay@protonmail.com>
2023-08-08 19:26:05 +0200

    instanciate character from Pillow Image

--- devan/character.py
+++ devan/character.py
@@ -1,16 +1,31 @@
-from joblib import load
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
 
 class Character:
-    def __init__(self, filename: str):
-        self.filename = filename
-        self.img_arr = self._read_image(filename)
+    def __init__(
+        self,
+        filename: str | None,
+        pil_img: Image.Image | None = None,
+    ):
+        if filename:
+            print(f"using filename {filename} for instanciate the Character object")
+            self.filename = filename
+            self.img_arr = self._array_from_file(filename)
+        elif pil_img:
+            self.filename = None
+            self.img_arr = self._array_from_pil(pil_img)
+        else:
+            raise NotImplementedError(
+                "not instanciating Character nor from file nor from PIL.Image"
+            )
 
-    def _read_image(self, filename: str) -> np.ndarray(shape=(32, 32), dtype=np.int32):
+    def _array_from_file(self, filename: str) -> np.ndarray:
         pil_img = Image.open(filename, "r")
+        return self._array_from_pil(pil_img)
+
+    def _array_from_pil(self, pil_img: Image.Image) -> np.ndarray:
         return np.array(pil_img, dtype=np.int32)
 
     def show_character(self):
