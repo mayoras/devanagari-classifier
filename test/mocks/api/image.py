f66fd3bb4994624daef110b77d8fceb1128187f7
Cesar Mayora <cesarmay@protonmail.com>
2023-08-08 19:32:18 +0200

    mocks examples module

---
+++ test/mocks/api/image.py
@@ -0,0 +1,16 @@
+from api.model import ImageBody
+
+DATA_FILENAME = "./test/mocks/api/data.txt"
+
+
+def get_mock_character() -> ImageBody:
+    fake_payload = {
+        "file": "image",
+        "mode": "gray",
+        "alpha": False,
+    }
+
+    with open(DATA_FILENAME, "r") as file:
+        fake_payload["data"] = file.read()
+
+    return ImageBody(**fake_payload)
