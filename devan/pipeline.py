66dca326af1f8a8986bd7fde9d309143de5214a6
Cesar Mayora <cesarmay@protonmail.com>
2023-08-14 20:21:04 +0200

    refactor: TransformList and Transform TypeAliases

--- devan/pipeline.py
+++ devan/pipeline.py
@@ -1,20 +1,26 @@
 import numpy as np
 
-from typing import Callable
+from typing import Callable, TypeAlias
 from devan.character import Character
+
+Transform: TypeAlias = tuple[Callable, dict]
+TransformList: TypeAlias = list[Transform]
 
 
 class Pipeline:
     # transformation === (func, {param1: p1, param2: p2})
-    def __init__(self, chars: list[Character], trans: list[tuple[Callable, dict]] = []):
+    def __init__(self, chars: list[Character], trans: TransformList = []):
         # init prediction set and transformations
         self.X: np.ndarray[int, np.dtype[np.float64]] = np.array(
             [c.img_arr for c in chars], dtype=np.float64
         )
         self.trans = trans
 
-    def add_transform(self, trans: tuple[Callable, dict]):
+    def add_transform(self, trans: Transform):
         self.trans.append(trans)
+
+    def add_multi_transform(self, trans: TransformList):
+        self.trans.extend(trans)
 
     def transform(self):
         # reshape the images to 1D-array of pixels
