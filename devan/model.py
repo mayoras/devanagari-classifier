fd524cc5e55e470d3b28f068f31b43a6b8a2ef0b
Cesar Mayora <cesarmay@protonmail.com>
2023-08-14 22:38:15 +0200

    label translation to predicted character name

--- devan/model.py
+++ devan/model.py
@@ -1,4 +1,5 @@
 import os
+from constants.character import LABELS_TRANS
 
 from joblib import load
 
@@ -10,8 +11,21 @@ class ModelObjectNotFound(Exception):
         self.message = f"Model object {filename} not found"
 
 
+class LabelError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
 def load_model(filename: str) -> MLPClassifier:
     if not os.path.exists(filename):
         raise ModelObjectNotFound(filename)
 
     return load(filename)
+
+
+def label_to_char_name(label: int) -> str:
+    if label not in range(0, len(LABELS_TRANS)):
+        raise LabelError(message=f"{label} is an invalid label.")
+
+    return LABELS_TRANS[label].split("_")[-1]
