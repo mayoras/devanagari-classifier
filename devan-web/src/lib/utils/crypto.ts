42ebe0239b97b9c23ace459303e444fc57219654
Cesar Mayora <cesarmay@protonmail.com>
2023-08-17 22:27:56 +0200

    generateIDs tested

--- devan-web/src/lib/utils/crypto.ts
+++ devan-web/src/lib/utils/crypto.ts
@@ -1,3 +1,11 @@
 import { randomUUID } from 'crypto';
 
-export function identifyPayloadImages() {}
+export function generateIDs(numIDs: number): string[] {
+	const ids: string[] = [];
+
+	for (let i = 0; i < numIDs; ++i) {
+		ids.push(randomUUID());
+	}
+
+	return ids;
+}
