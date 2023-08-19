81f69f78dbf50c88ac6a54b23966d206ee352b07
Cesar Mayora <cesarmay@protonmail.com>
2023-08-20 00:18:04 +0200

    check if incoming response is valid

--- devan-web/src/lib/utils/response.ts
+++ devan-web/src/lib/utils/response.ts
@@ -1,4 +1,12 @@
-type ResponseImageProps = devan.image.ResponseImageProps;
+type ResponseImageProps = {
+	success: boolean;
+	labels: [
+		{
+			id: string;
+			label: string;
+		}
+	];
+};
 
 export function isResponse(res: unknown): res is ResponseImageProps {
 	return res instanceof Object && 'success' in res && 'labels' in res;
