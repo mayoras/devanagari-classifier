af9c978db16e7060cf0af21c506171bfd3ac4ff7
Cesar Mayora <cesarmay@protonmail.com>
2023-08-19 20:44:55 +0200

    no predictions card

--- devan-web/src/routes/+page.svelte
+++ devan-web/src/routes/+page.svelte
@@ -18,6 +18,7 @@
 	let canvas: Canvas;
 	let pencilThickness = DEFAULT_PENCIL_THICKNESS;
 	let numCanvases = 1;
+	let predicted = false;
 
 	async function sendPayload(payload: PayloadImageProps[]) {
 		try {
@@ -66,37 +67,120 @@
 	}
 </script>
 
-<div class="container">
-	<div>
+<main class="main-container">
+	<div class="canvas-container">
 		<div>
-			<div class="pencil-ctls">
-				<div class="thk-icons">
-					<img src="/svg/Thin.svg" alt="Thin pencil" />
-					<img src="/svg/Medium.svg" alt="Medium pencil" />
-					<img src="/svg/Bold.svg" alt="Bold pencil" />
-				</div>
+			<div>
+				<div class="pencil-ctls">
+					<div class="thk-icons">
+						<img src="/svg/Thin.svg" alt="Thin pencil" />
+						<img src="/svg/Medium.svg" alt="Medium pencil" />
+						<img src="/svg/Bold.svg" alt="Bold pencil" />
+					</div>
 
-				<Slider
-					bind:value={pencilThickness}
-					min={MIN_PENCIL_THICKNESS}
-					max={MAX_PENCIL_THICKNESS}
-					numMarkers={NUM_THICKNESS_MARKERS}
+					<Slider
+						bind:value={pencilThickness}
+						min={MIN_PENCIL_THICKNESS}
+						max={MAX_PENCIL_THICKNESS}
+						numMarkers={NUM_THICKNESS_MARKERS}
+					/>
+				</div>
+				<Canvas
+					bind:this={canvas}
+					dims={{ width: IMG_WIDTH, height: IMG_HEIGHT, size: DRAW_POINTER_SIZE }}
+					thickness={pencilThickness}
 				/>
 			</div>
-			<Canvas
-				bind:this={canvas}
-				dims={{ width: IMG_WIDTH, height: IMG_HEIGHT, size: DRAW_POINTER_SIZE }}
-				thickness={pencilThickness}
-			/>
-		</div>
-		<div class="actions">
-			<Button type="button" label="Classify" on:click={handleExport} />
-			<Button type="button" label="Clear" on:click={() => canvas.clear()} />
+			<div class="actions">
+				<Button type="button" label="Classify" on:click={handleExport} />
+				<Button type="button" label="Clear" on:click={() => canvas.clear()} />
+			</div>
 		</div>
 	</div>
-</div>
+
+	<div
+		class="pred-container"
+		style="width: {IMG_WIDTH * DRAW_POINTER_SIZE}px; height: {IMG_HEIGHT * DRAW_POINTER_SIZE}px;"
+	>
+		<div class="card">
+			<div class="card-content">
+				{#if predicted}
+					<h2 class="pred-title">Your character is:</h2>
+					<img src="" alt="" />
+					<span class="pred-character-name"><i>Put character name</i></span>
+				{:else}
+					<h2 class="pred-title">You have not made a character prediction.</h2>
+					<img src="/svg/Brain.svg" alt="Gray Brain Icon" />
+					<span class="pred-no-character"
+						>Draw a character and click <code>Predict</code> to show prediction.</span
+					>
+				{/if}
+			</div>
+		</div>
+	</div>
+</main>
 
 <style>
+	.pred-container {
+		margin-top: 1em;
+		z-index: -1;
+	}
+
+	.card {
+		display: flex;
+		justify-content: center;
+
+		width: 100%;
+		height: 100%;
+		background-color: white;
+		border-radius: 2.5% 5% 5% 2.5%;
+
+		box-shadow: 0 0 7px 3px teal;
+	}
+
+	.card-content {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+
+		color: gray;
+		font-weight: 500;
+	}
+
+	.pred-title {
+		font-size: larger;
+		font-weight: bolder;
+		margin-bottom: 15px;
+	}
+
+	.card-content > img {
+		width: 150px;
+		height: 150px;
+		padding: 2em;
+	}
+
+	.main-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.canvas-container {
+		display: flex;
+		height: 90vh;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.actions {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-top: 2em;
+	}
+
 	.pencil-ctls {
 		display: flex;
 		flex-direction: column;
@@ -112,19 +196,5 @@
 	.thk-icons > img {
 		width: 2rem;
 		height: 3rem;
-	}
-
-	.container {
-		display: flex;
-		height: 100vh;
-		justify-content: center;
-		align-items: center;
-	}
-
-	.actions {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		margin-top: 2em;
 	}
 </style>
