3e1df4c53476f889bbc2a9bb0787ad087abba321
Cesar Mayora <cesarmay@protonmail.com>
2023-08-20 00:46:12 +0200

    add root layout and footer

---
+++ devan-web/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<slot />
+
+<footer><span>By Cesar A. Mayora</span></footer>
+
+<style>
+	footer {
+		display: flex;
+		justify-content: end;
+	}
+	span {
+		padding: 1em;
+		font-size: larger;
+		font-weight: lighter;
+		color: teal;
+	}
+</style>
