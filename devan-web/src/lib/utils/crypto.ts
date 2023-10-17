9df1fb281054ebcec56a626d349a919aae497c0e
Cesar Mayora <cesarmay@protonmail.com>
2023-10-17 15:53:53 +0200

    fix(crypto): default to crypto or polyfill

--- devan-web/src/lib/utils/crypto.ts
+++ devan-web/src/lib/utils/crypto.ts
@@ -1,5 +1,19 @@
 type IDGenerator = (() => string) | null;
 
+let defaultUUIDGenerator: () => string;
+
+if (typeof crypto === 'undefined' || !crypto.randomUUID) {
+	defaultUUIDGenerator = function () {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+			const r = (Math.random() * 16) | 0;
+			const v = c === 'x' ? r : (r & 0x3) | 0x8;
+			return v.toString(16);
+		});
+	};
+} else {
+	defaultUUIDGenerator = crypto.randomUUID;
+}
+
 export function generateIDs(numIDs: number, generator: IDGenerator = null): string[] {
 	const ids: string[] = [];
 
@@ -10,7 +24,7 @@ export function generateIDs(numIDs: number, generator: IDGenerator = null): stri
 		}
 	} else {
 		for (let i = 0; i < numIDs; ++i) {
-			ids.push(crypto.randomUUID());
+			ids.push(defaultUUIDGenerator());
 		}
 	}
 
